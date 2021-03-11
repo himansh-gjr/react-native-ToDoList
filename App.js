49122254f294adb1ae0bb4a803f107a912e1f7aa
himansh-gjr <himanshugjr1810@gmail.com>
2021-03-11 22:06:13 +0530

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,94 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import {
+  FlatList,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import Task from "./components/Task";
 
 export default function App() {
+  const [task, setTask] = useState("");
+  const [tasks, setTasks] = useState([]);
+
+  const handleAddTodo = () => {
+    const tasksLength = tasks.length + 1;
+    setTasks([...tasks, { task, id: tasksLength }]);
+    setTask("");
+  };
+
+  const handleRemoveTodo = (item) => {
+    setTasks(tasks.filter((taskItem) => taskItem.id !== item.id));
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={{ padding: 20 }}>
+        <Text style={styles.text}>Your Tasks</Text>
+      </View>
+      <View>
+        <FlatList
+          data={tasks}
+          keyExtractor={(item) => item.id.toString()}
+          renderItem={({ item }) => (
+            <Task item={item} onPress={() => handleRemoveTodo(item)} />
+          )}
+        />
+      </View>
+      <View style={styles.downContainer}>
+        <TextInput
+          onChangeText={(text) => setTask(text)}
+          value={task}
+          style={styles.textInput}
+          placeholder="type your task here"
+        />
+        <TouchableOpacity onPress={handleAddTodo}>
+          <View style={styles.iconContainer}>
+            <MaterialCommunityIcons name="plus" size={24} color="#E8EAED" />
+          </View>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    marginTop: 30,
+    backgroundColor: "#E8EAED",
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  iconContainer: {
+    backgroundColor: "#55BCF6",
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    fontSize: 30,
+    fontStyle: "italic",
+  },
+  downContainer: {
+    width: "100%",
+    flexDirection: "row",
+    alignItems: "center",
+    padding: 15,
+    position: "absolute",
+    bottom: 0,
+    backgroundColor: "grey",
+  },
+  textInput: {
+    backgroundColor: "#FFF",
+    borderRadius: 20,
+    width: "80%",
+    margin: 5,
+    height: 50,
+    padding: 10,
   },
 });
---
+++ components/Task.js
@@ -0,0 +1,48 @@
+import React from "react";
+import { View, StyleSheet, Text, TouchableWithoutFeedback } from "react-native";
+
+function Task({ item, onPress }) {
+  return (
+    <TouchableWithoutFeedback onPress={onPress}>
+      <View style={styles.container}>
+        <View style={styles.square} />
+        <View style={styles.textContainer}>
+          <Text style={{ fontSize: 15 }}>{item.task}</Text>
+        </View>
+        <View style={styles.circle}></View>
+      </View>
+    </TouchableWithoutFeedback>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    backgroundColor: "#FFF",
+    alignItems: "center",
+    justifyContent: "space-between",
+    margin: 20,
+    height: 50,
+    borderRadius: 5,
+    padding: 10,
+  },
+  square: {
+    width: 24,
+    height: 24,
+    backgroundColor: "#55BCF6",
+    opacity: 0.4,
+    borderRadius: 5,
+  },
+  circle: {
+    width: 12,
+    height: 12,
+    borderColor: "#55BCF6",
+    borderWidth: 2,
+    borderRadius: 5,
+  },
+  textContainer: {
+    width: "80%",
+  },
+});
+
+export default Task;
